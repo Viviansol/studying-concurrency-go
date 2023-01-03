a5ff172859f0e8fbc30691e94b5fa27599bd16eb
Viviansol <viviansan93@gmail.com>
2023-01-03 18:52:52 -0300

    wait groups

---
+++ first_module/waitGroups/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	words := []string{
+		"alpha", "beta", "delta", "gama", "pi", "zeta", "eta", "theta", "episolon",
+	}
+	wg.Add(9)
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+	wg.Wait()
+	wg.Add(1)
+	time.Sleep(1 * time.Second)
+	printSomething("second", &wg)
+}
